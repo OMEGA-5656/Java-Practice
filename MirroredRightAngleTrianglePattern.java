364966f28f2bfb6bd386f580c6542b197ab9a6cb
AVNISH SHARMA <abhisheksaini642794@gmail.com>
2026-02-20 09:28:08 +0530

    Mirrored Right Angle * printing pattern

--- MirroredRightAngleTrianglePattern.java
+++ MirroredRightAngleTrianglePattern.java
@@ -1,11 +1,14 @@
 import java.util.*;
-public class RightAngleTrianglePattern {
+public class MirroredRightAngleTrianglePattern {
     public static void main(String main[]) {
         System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for(int i = 1; i<num+1;i++) {
-            for(int j = 0; j<i ; j++){
+        for(int i = 1; i<num+1 ; i++) {
+            for(int j = num-i; j>0 ; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=i; k++){
                 System.out.print("*");
             }
             System.out.println();
