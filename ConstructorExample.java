56b16f03707d1fe56aa129e163b6c38b8b4c8068
AVNISH SHARMA <abhisheksaini642794@gmail.com>
2026-02-20 15:52:20 +0530

    Constructor Example

---
+++ ConstructorExample.java
@@ -0,0 +1,14 @@
+public class ConstructorExample {
+    public int x;
+    ConstructorExample(){
+        System.out.println("Constructor Called.");
+        this.x = x;
+    }
+    void Example(int a){
+        System.out.println(a);
+    }
+    public static void main(String args[]){
+        ConstructorExample ob = new ConstructorExample();
+        ob.Example(19);
+    }
+}
