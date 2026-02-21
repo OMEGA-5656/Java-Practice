4d814038d12a760c1597f6796ae13f63c1f8dfa3
AVNISH SHARMA <abhisheksaini642794@gmail.com>
2026-02-21 15:45:54 +0530

    access modifiers

---
+++ practice/AccessModifiers.java
@@ -0,0 +1,8 @@
+package practice;
+import practice.Person;
+public class AccessModifiers {
+    public static void main(String args[]){
+        System.out.println("Name: "+Person.name);
+        System.out.println("Age: "+Person.age);
+    }
+}
---
+++ practice/Person.java
@@ -0,0 +1,6 @@
+package practice;
+
+public class Person {
+    static protected String name = "xyz";
+    static protected int age = 19;
+}
