771b66725670bf7912cff2417555e40d7e823f63
AVNISH SHARMA <abhisheksaini642794@gmail.com>
2026-03-05 11:47:43 +0530

    deadlocks

---
+++ Deadlock.java
@@ -0,0 +1,21 @@
+class A{}
+class B{}
+public class Deadlock {
+    static A a = new A();
+    static B b = new B();
+    public static void main(String args[]){
+        Thread t1 = new Thread(()->{
+            synchronized (a){
+                System.out.println("Thread 1 locked ObjA");
+                try{
+                    Thread.sleep(10000);
+                }
+                catch(Exception e){}
+                System.out.println("Thread 1 waiting for ObjB");
+                synchronized (b){
+                    System.out.println("Thread 1 locked objB");
+                }
+            }
+        });
+    }
+}
