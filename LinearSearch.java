dcdd81888ebc8f0fdce30ea7aba17af53a9d083b
AVNISH SHARMA <abhisheksaini642794@gmail.com>
2026-03-06 09:29:41 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LinearSearch {
+    public static int linearSearch(int[]arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int[] arr = {5,66,443,1,4,532,553,2,11,23};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the search element: ");
+        System.out.println("Found at index: "+linearSearch(arr,sc.nextInt()));
+    }
+}
