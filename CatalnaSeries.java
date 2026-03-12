2c79228f683efbafee3ac9cc5b517e5afda7dc57
OMEGA-5656 <abhisheksaini642794@gmail.com>
2026-03-12 10:49:48 +0530

    add catalan series

---
+++ CatalnaSeries.java
@@ -0,0 +1,18 @@
+class CatalanSeries{
+    static int catalan(int n){
+        if(n==0||n==1)return 1;
+        int result = 0;
+        for(int i=0;i<n;i++){
+            result+=catalan(i)*catalan(n-1-i);
+        }
+        return result;
+
+    }
+    public static void main(String args[]){
+        int terms = 10;
+        System.out.println("Catalan Number series:");
+        for(int i=0;i<terms;i++){
+            System.out.println(catalan(i)+" ");
+        }
+    }
+}
